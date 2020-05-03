c568969b9c3973cf52747b52275563f2747c4555
Robert Pym <rob_pym@hotmail.com>
2020-05-03 15:41:34 +0100

    Initial failing test for Storage domain entity

---
+++ StoreCupboardDomain.Test/StorageTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace StoreCupboardDomain.Test
+{
+    /// <summary>
+    /// Unit tests for <see cref="Storage"/>
+    /// </summary>
+    [TestFixture]
+    internal class StorageTests
+    {
+        [Test]
+        public void WhenConstructed_ThenExpectedProperties(
+            [Values("", "Larder", "Fridge")] string name)
+        {
+            // Act
+            var target = new Storage(name);
+
+            // Assert
+            Assert.That(target.Id, Is.Not.Empty);
+            Assert.That(target.Name, Is.EqualTo(name));
+        }
+    }
+}
---
+++ StoreCupboardDomain/Storage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreCupboardDomain
+{
+    /// <summary>
+    /// Domain entity for Storage
+    /// </summary>
+    internal class Storage
+    {
+    }
+}
